bee0bf4dc4cd4d596434a7c5bd0cbbbe7eb25120
miguelferia <miguelferia@gmail.com>
2020-11-12 13:36:46 +0800

    Default yes to installation prompts

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt-key add /var/nvidia-driver-local-repo-418.87.01/7fa2af80.pub
 dpkg -i nvidia-driver-local-repo-ubuntu1804-418.87.01_1.0-1_amd64.deb
 
 apt update
-apt install cuda-drivers
+apt install cuda-drivers -y
 
 snap install blender --classic
-apt install libgl1-mesa-glx libxi6 libxrender1 nvtop
+apt install libgl1-mesa-glx libxi6 libxrender1 nvtop -y
